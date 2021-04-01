eb5dee3557391b3e8a233356b7d466116cab11a8
fou <fou@benevity.com>
2021-03-31 23:01:07 -0600

    - Add CORS support

--- src/main/java/com/jarvis/adminservice/controller/PingController.java
+++ src/main/java/com/jarvis/adminservice/controller/PingController.java
@@ -2,10 +2,12 @@ package com.jarvis.adminservice.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/public")
 public class PingController {
 
     @GetMapping("/ping")
