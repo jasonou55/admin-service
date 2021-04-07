ff89d62eaae742635ea8e54004488773cd98f167
fou <fou@benevity.com>
2021-04-06 21:59:13 -0600

    - Fix organization endpoint error

--- src/main/java/com/jarvis/adminservice/controller/OrganizationController.java
+++ src/main/java/com/jarvis/adminservice/controller/OrganizationController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("~/management")
+@RequestMapping("/management")
 public class OrganizationController extends GenericController<Organization, OrganizationRequest, OrganizationResponse, OrganizationService> {
 
     @Autowired OrganizationService organizationService;
